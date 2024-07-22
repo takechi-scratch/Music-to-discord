7470c47878986d239be5ea502f160514a661845d
takechi-scratch <takechi.scratch@gmail.com>
2024-07-22 18:45:46 +0900

    いろいろ詳しく

---
+++ README.md
@@ -0,0 +1,20 @@
+# Music-to-Discord
+ブラウザで再生中の音楽を、Discordの「リッチプレゼンス」で表示できます。
+
+## 導入方法
+1. ファイルをダウンロードします。
+2. ファイル内の「extension」を、ブラウザの拡張機能として読み込みます。
+（やり方は調べてください）
+3. 「app.py」を実行すると、拡張機能と接続されてDiscordに送信されます。
+
+## ライセンス
+コードには [Socket.IO](https://socket.io/)（MITライセンス）が含まれています。
+本当にありがとうございます。
+
+たーけが製作したコードはMITライセンスです。二次利用する際は、条件をよくご確認ください。
+
+## 製作者
+[@takechi-scratch](https://scratch.mit.edu/users/takechi-scratch)
+- Scratchでは活動休止中
+- 最近Javascriptを勉強中
+- アプリごとに仮想環境を作るのが面倒
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import time
-from concurrent.futures import ThreadPoolExecutor
+from math import floor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from pypresence import Presence
 from flask import Flask
@@ -12,6 +13,8 @@ app.config.from_object(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 title = "未再生"
 artist = "未再生"
+last_set = 0
+last_get = 0
 
 
 @app.route("/", methods=["GET"])
@@ -24,12 +27,12 @@ def home():
 def handle_message(data):
     global title
     global artist
-
-    print(data)
+    global last_get
 
     if data not in ["Ping", "Hello"]:
         title = data["title"]
         artist = data["artist"]
+        last_get = time.time()
 
         emit("message", json.dumps(data))
 
@@ -37,13 +40,15 @@ def handle_message(data):
 def rpc_run():
     global title
     global artist
+    global last_set
 
     client_id = "1264115283140804628"
     RPC = Presence(client_id, pipe=0)
     RPC.connect()
 
     while True:
-        print(RPC.update(details=title, state=artist, large_image="http://takechi.starfree.jp/Line_Music.png"))  # Set the presence
+        RPC.update(details=title, state=artist, large_image="http://takechi.starfree.jp/Line_Music.png")
+        last_set = time.time()
         time.sleep(15)
 
 
@@ -52,8 +57,33 @@ def flask_run():
 
 
 if __name__ == "__main__":
-    with ThreadPoolExecutor(max_workers=2) as executor:  # 同時実行できるスレッド数の上限を設定
+    with ThreadPoolExecutor(max_workers=2) as executor:
         futures = {executor.submit(task): task for task in [
             rpc_run,
             flask_run
         ]}
+        print("起動ができました！少々お待ちください…")
+        print("-" * 30 + "\n\n", end="")
+        print()
+
+        while True:
+            print("\033[2A", end="")
+            if time.time() - last_get < 10:
+                print(f"正常に稼働中 最終設定:{floor(time.time() - last_set)}秒前 最終取得:{floor(time.time() - last_get)}秒前                           ")
+            elif time.time() - last_set < 30:
+                print("曲名を取得していません。Youtubeを開いて再生してみてください。                                      ")
+            else:
+                print("設定が行われていません。プログラムを再起動してください。                            ")
+            print(f"曲名:{title} , チャンネル:{artist}                                                                      ")
+
+            time.sleep(0.5)
+
+            # input("あああ")
+
+    # タスクが完了するまで待機
+    for future in as_completed(futures):
+        task = futures[future]
+        try:
+            print("正常に終了しました。")
+        except Exception as e:
+            logger.critical(f"エラーが発生しました（エラー: {e}, タスク: {task})")
