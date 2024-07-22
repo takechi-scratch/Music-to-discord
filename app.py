2baf438ed5afaa9642fce4107465fca0ba205ecd
takechi-scratch <takechi.scratch@gmail.com>
2024-07-22 18:55:12 +0900

    コードをすっきり

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import time
 from math import floor
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 
 from pypresence import Presence
 from flask import Flask
@@ -30,8 +30,8 @@ def handle_message(data):
     global last_get
 
     if data not in ["Ping", "Hello"]:
-        title = data["title"]
-        artist = data["artist"]
+        title = data["title"] if data["title"] == "" else "未再生"
+        artist = data["artist"] if data["artist"] == "" else "未再生"
         last_get = time.time()
 
         emit("message", json.dumps(data))
@@ -69,21 +69,15 @@ if __name__ == "__main__":
         while True:
             print("\033[2A", end="")
             if time.time() - last_get < 10:
-                print(f"正常に稼働中 最終設定:{floor(time.time() - last_set)}秒前 最終取得:{floor(time.time() - last_get)}秒前                           ")
+                print("正常に稼働中",
+                      f"最終設定:{floor(time.time() - last_set)}秒前",
+                      f"最終取得:{floor(time.time() - last_get)}秒前" + " "*30)
             elif time.time() - last_set < 30:
-                print("曲名を取得していません。Youtubeを開いて再生してみてください。                                      ")
+                print("曲名を取得していません。Youtubeを開いて再生してみてください。" + " "*30)
             else:
-                print("設定が行われていません。プログラムを再起動してください。                            ")
-            print(f"曲名:{title} , チャンネル:{artist}                                                                      ")
+                print("設定が行われていません。プログラムを再起動してください。" + " " * 30)
+            print(f"曲名:{title} , チャンネル:{artist}" + " " * 30)
 
             time.sleep(0.5)
 
             # input("あああ")
-
-    # タスクが完了するまで待機
-    for future in as_completed(futures):
-        task = futures[future]
-        try:
-            print("正常に終了しました。")
-        except Exception as e:
-            logger.critical(f"エラーが発生しました（エラー: {e}, タスク: {task})")
--- extension/content.js
+++ extension/content.js
@@ -3,18 +3,24 @@ const socket = io.connect("http://localhost:8080");
 // YouTube用
 if (window.location.host.includes('youtube.com')) {
     setInterval(() => {
-        console.log("title取得");
-        let titleElement = document.querySelector("#title > h1 > yt-formatted-string");
-        let artistElement = document.querySelector("#text > a")
-        if (titleElement) {
-            console.log(titleElement.innerText);
+        if (window.location.href.includes('youtube.com/watch')) {
+            let titleElement = document.querySelector("#title > h1 > yt-formatted-string");
+            let artistElement = document.querySelector("#text > a")
+            if (titleElement) {
+                console.log(titleElement.innerText);
+            }
+        } else {
+            let titleElement = ""
+            let artistElement = ""
         }
-        socket.emit("message", {"title": titleElement.innerText, "artist": artistElement.innerText});
-        }, 1000);
-        console.log("YouTube");
+        data = { "title": titleElement.innerText, "artist": artistElement.innerText }
+        socket.emit("message", data);
+        console.log(data)
+    }, 1000);
+    console.log("YouTube");
 
-        // 操作画面と接続できたとき
-    socket.on("connect", function() {
+    // 操作画面と接続できたとき
+    socket.on("connect", function () {
         console.log("WebSocketの接続完了");
         socket.send("Hello");
 
